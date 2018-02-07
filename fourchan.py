4c41345b00960a70ad3438cda6f9880af610acd6
Jorde Kang <jorde@protonmail.ch>
2018-02-07 23:13:15 +0000

    complete rewrite using API

---
+++ fourchan.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+def getThread(board, thread):
+    r = requests.get("http://a.4cdn.org/"+board+"/thread/"+thread+".json")
+    
+
+def getImages(board, thread):
+    print("kek, dis shit ain\'t supported yet nigga")
+    
---
+++ reader.py
@@ -0,0 +1,16 @@
+from fourchan import *
+import sys
+
+
+helpmsg = "Welcome to yur3i\'s 4chan reader!\n\nUSAGE: -t/i <board> <thread>\n-t get all text posts form thread\n-i get all images from the thread and dump them in the current directory"
+
+if len(sys.argv) == 1:
+    print(helpmsg)
+if sys.argv[1] in ["-h"]:
+    print(helpmsg)
+    quit()
+if sys.argv[1] in ["-t"]:
+    getThread(sys.argv[2], sys.argv[3])
+if sys.argv[1] in ["-i"]:
+    getImages(sys.argv[2], sys.argv[3])
+

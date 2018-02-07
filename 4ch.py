569d3533397809b2d66c2bc3e9cfefccc022e69a
Jorde Kang <jorde@protonmail.ch>
2018-02-07 00:57:55 +0000

    Text posts now work

--- 4ch.py
+++ 4ch.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
 import requests
+import re
 from bs4 import BeautifulSoup as bsoup
 
 helpmsg = "Welcome to yur3i\'s 4chan reader, this help message was displayed because you passed no arguments\nUSAGE: 4ch.py -t/-i <board> <thread>\n-t get all posts in a thread as text.\n-i download all the images from a thread."
@@ -9,13 +10,17 @@ def getimages(board, thread):
     r = "http://www.boards.4chan.org/"+board+"/thread/"+thread
     site = r.text
     soup = bsoup(site, "html5lib")
+
 def getposts(board, thread):
     print("getting posts from www.4chan.org/"+board+"/thread/"+thread+"...")
     r = requests.get("http://boards.4chan.org/"+board+"/thread/"+thread)
     site = r.text
     soup = bsoup(site, "html5lib")
-    op = soup.find_all("blockquote", {"class": "postMessage"})
-    print(op)
+    for post in soup.find_all("blockquote", {"class": "postMessage"}):
+        postid = post.get("id")
+        postcontent = post.contents
+        print(">"+str(postid[1:])+"\n"+str(postcontent)[2:-2]+"\n")
+
 #if no arguments were passed, display the help message
 if len(sys.argv) == 1:
     print(helpmsg)

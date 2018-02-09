a641d3c425a9d6dfa0cdecc5840d2830c8b83df9
Jorde Kang <jorde@protonmail.ch>
2018-02-09 14:16:35 +0000

    used regex to strip html

--- fourchan.py
+++ fourchan.py
@@ -1,4 +1,11 @@
+import re
 import requests
+
+def cleanhtml(content):
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, '', content)
+    return cleantext
+    
 
 def getThread(board, threadno):
     r = requests.get("http://a.4cdn.org/"+board+"/thread/"+threadno+".json")
@@ -7,9 +14,10 @@ def getThread(board, threadno):
     i = 0
     for i in range(0, threadreplies+1):
         postnum = threadjson["posts"][i]["no"]
-        postcontent = threadjson["posts"][i]["com"]
-        print("\n>"+str(postnum)+"\n"+postcontent+"\n")
+        postcontent = str(threadjson["posts"][i]["com"])
+        parsedContent = cleanhtml(postcontent)
+        print("\n>"+str(postnum)+"\n"+str(parsedContent)+"\n")
 
 def getImages(board, thread):
     print("kek, dis shit ain\'t supported yet nigga")
-    
+
